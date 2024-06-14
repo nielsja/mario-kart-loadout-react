8f144ed9fae567912a7846605f3ffd0ad1f99053
Jennifer Nielsen <nielsja13@gmail.com>
2024-06-14 16:25:25 -0500

    init tools

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,15 +4,40 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'plugin:import/recommended',
+    'plugin:prettier/recommended', // needs to be last
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['import', 'react-refresh'],
   rules: {
+    'import/newline-after-import': 2,
+    'import/no-duplicates': [2],
+    'import/order': [
+      2,
+      {
+        alphabetize: { order: 'asc' },
+      },
+    ],
+    'prettier/prettier': ['warn'],
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
   },
-}
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: true,
+      node: true,
+    },
+    react: {
+      version: 'detect',
+    },
+  },
+};
