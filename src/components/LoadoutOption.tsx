797501f1f699a8f686fc5c124dda1ad469f34954
Jennifer Nielsen <nielsja13@gmail.com>
2024-06-26 13:06:30 -0500

    updated layout for dropdown items in selector

--- src/components/LoadoutOption.tsx
+++ src/components/LoadoutOption.tsx
@@ -12,31 +12,34 @@ interface LoadoutOption {
 function LoadoutOption({ options, onSelectOption }: LoadoutOptionProps) {
   return (
     <>
-      <div className="dropdown w-100">
-        <button
-          className="btn btn-secondary dropdown-toggle w-100"
-          type="button"
-          data-bs-toggle="dropdown"
-          aria-expanded="false"
-        >
-          Dropdown button
-        </button>
-        <ul className="dropdown-menu dropdown-menu-lg-start dropdown-menu-sm-center">
-          {options.map((opt) => {
-            return (
-              <li
-                className="dropdown-item-li"
-                key={`loadout-option-${opt.optionId}`}
-                onClick={() => onSelectOption(opt.optionId)}
-              >
-                <a className="dropdown-item" href="#">
-                  <img className="dropdown-item-image" src={opt.imageUrl}></img>
-                  {/* <span>{opt.optionId}</span> */}
-                </a>
-              </li>
-            );
-          })}
-        </ul>
+      {' '}
+      <div className="m-3">
+        <div className="dropdown extended w-100">
+          <button
+            className="btn btn-secondary dropdown-toggle w-100"
+            type="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            Dropdown button
+          </button>
+          <ul className="dropdown-menu dropdown-menu-lg-start dropdown-menu-sm-center">
+            {options.map((opt) => {
+              return (
+                <li
+                  className="dropdown-item-li"
+                  key={`loadout-option-${opt.optionId}`}
+                  onClick={() => onSelectOption(opt.optionId)}
+                >
+                  <a className="dropdown-item" href="#">
+                    <img src={opt.imageUrl}></img>
+                    {/* <span>{opt.optionId}</span> */}
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>{' '}
       </div>
     </>
   );
