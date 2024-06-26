a707d9f7f7de205c35026362d102fdddbece1754
Jennifer Nielsen <nielsja13@gmail.com>
2024-06-26 14:39:11 -0500

    fixed selected card image sizing and layout

--- src/components/SelectedOption.tsx
+++ src/components/SelectedOption.tsx
@@ -10,9 +10,8 @@ function SelectedOption({ cardTitle, imageUrl }: SelectedOptionProps) {
     <>
       <div className="m-3">
         <div className="card card-contents w-100">
-          <img src={imageUrl} className="card-image"></img>
           <div className="card-body">
-            <h5 className="card-title">{cardTitle}</h5>
+            <img src={imageUrl} className="card-image" alt={cardTitle}></img>
           </div>
         </div>
       </div>
