294d1bb58049df4a491f2f482bfc99315cc690fa
Jennifer Nielsen <nielsja13@gmail.com>
2024-07-12 16:38:19 -0500

    removed unnecessary useEffect

--- src/components/Stats/AllStats.tsx
+++ src/components/Stats/AllStats.tsx
@@ -1,4 +1,3 @@
-import { useEffect } from 'react';
 import { getCharacter } from '../../entities/Characters';
 import { getGlider } from '../../entities/Gliders';
 import { getKart } from '../../entities/Karts';
@@ -15,19 +14,11 @@ export interface AllStatsProps {
 }
 
 function AllStats({ driver, kart, tire, glider }: AllStatsProps) {
-  // const [driverStats, setDriverStat] = useState(
-  //   driver ? getCharacter(driver).stats : null
-  // );
-  // const [kartStats, setKartStat] = useState(kart ? getKart(kart).stats : null);
-  // const [tireStats, setTireStat] = useState(tire ? getTire(tire).stats : null);
-  // const [gliderStats, setGliderStat] = useState(
-  //   glider ? getGlider(glider).stats : null
-  // );
-  let driverStats: IStats | null = getCharacterStats(driver);
-  let kartStats: IStats | null = getKartStats(kart);
-  let tireStats: IStats | null = getTireStats(tire);
-  let gliderStats: IStats | null = getGliderStats(glider);
-  let overallStats: IStats = calculateAllStats(
+  let driverStats: IStats | null = null;
+  let kartStats: IStats | null = null;
+  let tireStats: IStats | null = null;
+  let gliderStats: IStats | null = null;
+  let overallStats = calculateAllStats(
     driverStats,
     kartStats,
     tireStats,
@@ -35,11 +26,29 @@ function AllStats({ driver, kart, tire, glider }: AllStatsProps) {
   );
   let displayStats: IStats = calculateAllDisplayStats(overallStats);
 
-  useEffect(() => {
-    driverStats = getCharacterStats(driver);
-    kartStats = getKartStats(kart);
-    tireStats = getTireStats(tire);
-    gliderStats = getGliderStats(glider);
+  function getCharacterStats(characterId: string) {
+    driverStats = characterId ? getCharacter(driver).stats : null;
+    updateOverallStats();
+    return driverStats;
+  }
+  function getKartStats(kartId: string) {
+    kartStats = kartId ? getKart(kart).stats : null;
+    updateOverallStats();
+    return kartStats;
+  }
+
+  function getTireStats(tireId: string) {
+    tireStats = tireId ? getTire(tire).stats : null;
+    updateOverallStats();
+    return tireStats;
+  }
+
+  function getGliderStats(gliderId: string) {
+    gliderStats = gliderId ? getGlider(gliderId).stats : null;
+    updateOverallStats();
+    return gliderStats;
+  }
+  function updateOverallStats() {
     overallStats = calculateAllStats(
       driverStats,
       kartStats,
@@ -47,34 +56,15 @@ function AllStats({ driver, kart, tire, glider }: AllStatsProps) {
       gliderStats
     );
     displayStats = calculateAllDisplayStats(overallStats);
-
-    console.log('character weight: ', driverStats?.weight);
-    console.log('kart weight: ', kartStats?.weight);
-    console.log('tire weight: ', tireStats?.weight);
-    console.log('glider weight: ', gliderStats?.weight);
-  }, [driver, kart, tire, glider]);
-  function getCharacterStats(characterId: string) {
-    return characterId ? getCharacter(driver).stats : null;
-  }
-  function getKartStats(kartId: string) {
-    return kartId ? getKart(kart).stats : null;
-  }
-
-  function getTireStats(tireId: string) {
-    return tireId ? getTire(tire).stats : null;
-  }
-
-  function getGliderStats(gliderId: string) {
-    return gliderId ? getGlider(gliderId).stats : null;
   }
 
   return (
     <>
       <div className="container-fluid">
-        <p>character stat: {driverStats?.weight}</p>
-        <p>kart stat: {kartStats?.weight ?? 'none'}</p>
-        <p>tire stat: {tireStats?.weight ?? 'none'}</p>
-        <p>glider stat: {gliderStats?.weight ?? 'none'}</p>
+        <p>character stat: {getCharacterStats(driver)?.weight ?? 'none'}</p>
+        <p>kart stat: {getKartStats(kart)?.weight ?? 'none'}</p>
+        <p>tire stat: {getTireStats(tire)?.weight ?? 'none'}</p>
+        <p>glider stat: {getGliderStats(glider)?.weight ?? 'none'}</p>
         <p>overall stat: {overallStats.weight}</p>
         <p>displayed bar %: {displayStats.weight}</p>
         <StatBar statName="Speed" displayValue={displayStats.weight}></StatBar>
