bc6c2d622bfba5b2f7ec7eb87df986cb421bf981
Jennifer Nielsen <nielsja13@gmail.com>
2024-07-13 09:04:39 -0500

    added in all driver, kart, glider, tire stats

--- src/components/Stats/IStats.ts
+++ src/components/Stats/IStats.ts
@@ -1,17 +1,16 @@
 export interface IStats {
   weight: number;
   acceleration: number;
-  // TODO: add these back in
-  // onRoadTraction: number;
-  // offRoadTraction: number;
-  // miniTurbo: number;
-  // groundSpeed: number;
-  // waterSpeed: number;
-  // antiGravitySpeed: number;
-  // airSpeed: number;
-  // groundHandling: number;
-  // waterHandling: number;
-  // antiGravityHandling: number;
-  // airHandling: number;
-  // invincibility: number;
+  onRoadTraction: number;
+  offRoadTraction: number;
+  miniTurbo: number;
+  groundSpeed: number;
+  waterSpeed: number;
+  antiGravitySpeed: number;
+  airSpeed: number;
+  groundHandling: number;
+  waterHandling: number;
+  antiGravityHandling: number;
+  airHandling: number;
+  invincibility: number;
 }
